88f3c68da74c2b01d911ed61aa496d2cccb72940
Jens CN <jens.sydaker@gmail.com>
2024-09-09 15:24:10 +0200

    fixade css till brädet och settings

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -4,7 +4,7 @@ import "./Board.css";
 const rows = 6;
 const columns = 7;
 const playerX = "X";
-const player0 = "0";
+const player0 = "O";
 
 type Grid = string[][];
 
@@ -16,51 +16,45 @@ interface BoardProps {
 const createGrid = (): Grid =>
   Array.from({ length: rows }, () => Array(columns).fill(" "));
 
-//Håller koll på grid och nuvarande spelare
+// Board component
 export default function Board(props: BoardProps) {
   const [grid, setGrid] = useState<Grid>(createGrid);
   const [currentPlayer, setCurrentPlayer] = useState<string>(playerX);
   const [winner, setWinner] = useState<string | null>(null);
 
-  //Placerar bricka i specifik column
   const placeToken = (column: number): void => {
     if (winner || !canPlaceToken(column)) return;
 
     const newGrid = [...grid];
-    //Hittar första lediga cellen i column
     for (let row = rows - 1; row >= 0; row--) {
       if (newGrid[row][column] === " ") {
         newGrid[row][column] = currentPlayer;
         setGrid(newGrid);
-
-        //Kollar om nuvarande spelare vinner ----- Måste mergas ihop med Jens WinChecker
-        // if (checkWin(currentPlayer, newGrid)) {
-        //   setWinner(currentPlayer);
-        // } else {
-        //   setCurrentPlayer(currentPlayer === playerX ? player0 : playerX);
-        //  }
-        //  break;
+        setCurrentPlayer(currentPlayer === playerX ? player0 : playerX);
+        break;
       }
     }
   };
-  //Kollar om en bricka kan bli placerad i specifik column
+
   const canPlaceToken = (column: number): boolean => grid[0][column] === " ";
 
-  //Resettar spelet
   const resetGame = (): void => {
     setGrid(createGrid());
     setCurrentPlayer(playerX);
     setWinner(null);
   };
 
-  //Renderar grid och kontrollerar spelet
   const renderGrid = (): React.ReactNode => {
     return grid.map((row, rowIndex) => (
       <React.Fragment key={rowIndex}>
         {row.map((cell, colIndex) => (
-          <React.Fragment key={colIndex}>
-            <button onClick={() => placeToken(colIndex)}>{cell}</button>
-          </React.Fragment>
+          <button
+            key={colIndex}
+            className="cell"
+            onClick={() => placeToken(colIndex)}
+          >
+            {cell}
+          </button>
         ))}
       </React.Fragment>
     ));
@@ -68,9 +62,11 @@ export default function Board(props: BoardProps) {
 
   return (
     <div>
-      {renderGrid()}
+      <div className="grid-container">{renderGrid()}</div>
       {winner && <p>{`Player ${winner} wins!`}</p>}
-      <button onClick={resetGame}>Reset</button>
+      <div className="reset-btn">
+        <button onClick={resetGame}>Reset</button>
+      </div>
     </div>
   );
 }
