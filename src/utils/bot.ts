cecae93361bde7612e6121db95182828fcc710fc
Svintoo <S_maskuniitty@hotmail.se>
2024-09-10 22:06:44 +0200

    Hard bot klar, saknar lite felhantering bara

--- src/utils/bot.ts
+++ src/utils/bot.ts
@@ -19,19 +19,37 @@ export default function botMove(grid: Grid, difficulty: string): string {
 function findOptimalMove(grid: Grid): string {
   //Just nu kommer botten bara spela som "O", måste skriva om sen för att kunna
   //spela botvsbot
-  //check column from right to left, check from top to bottom row
+  //Kollar columner från höger till vänster, uppifrån ner
   for (let col = 6; col >= 0; col--) {
     console.log("checked column " + col);
     for (let row = 0; row <= 5; row++) {
       //console.log("checked row " + row);
-      if (grid[row][col] !== " " && grid[row][col] !== "X") {
-        let move = row + "," + (col - 1); //här gör jag logiskt fel, den gör movet
-        //på samma ställe som ett potentiellt X ändå, får skriva mer logik ToDo
-        return move;
+      if (grid[row][col] !== " " && grid[row][col] === "O") {
+        //top nivå, hitta första förekomster av symbol
+        let moveCandidates = [];
+        //titta över och kolla om den är tom
+        if (grid[row - 1][col] === " ") {
+          moveCandidates.push([col, row - 1]);
+        }
+        //titta till vänster och under om samma symbol, får skriva extra kod utifall [row-1 är undefined]
+        if (grid[row][col - 1] === " " && grid[row + 1][col - 1] !== " ") {
+          moveCandidates.push([col - 1, row]);
+        }
+        //titta till höger och under om samma symbol
+        if (grid[row][col + 1] === " " && grid[row + 1][col + 1] !== " ") {
+          moveCandidates.push([col + 1, row]);
+        }
+        //om den fick 3 olika kandidater så slumpa vart den lägger ny bricka, kolla också så att den inte är tom
+        if (moveCandidates.length !== 0) {
+          let rand = getRandomInt(moveCandidates.length);
+          return String(moveCandidates[rand]);
+        }
+        //ToDo, felhantering av out of bounds och generell felhantering
+        console.log(moveCandidates);
+        return "temp";
       }
     }
   }
-  //check row
   return "none";
 }
 
