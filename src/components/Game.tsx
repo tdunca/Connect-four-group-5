2fd9d36c09c8741eac41ace82e8ba106977d2b18
mariagryp <amnotyours@gmail.com>
2024-09-10 15:35:29 +0200

    back to the previouse view

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,7 +1,9 @@
-import { useState, useEffect } from "react";
-//import { Player } from "../klasser/Player";
-import Board from "../components/Board_1";
+import React, { useState, useEffect } from "react";
+import { Player } from "../klasser/Player";
+import Board from "../components/Board";
 import "./Game.css";
+//import Board from "../klasser/Board";
+//import { validateInput } from "../utils/validateInput";
 import { useWinCheck } from "../utils/WinCheck"; // Import the win check hook
 import { Options } from "../klasser/Options";
 
@@ -11,20 +13,31 @@ interface GameProps {
 
 export default function Game(props: GameProps) {
   //skrev om från React.FC till default function för att kunna ta emot props från settings
+  // a vertical board 7 columns across and 6 rows high
   const rows = 6;
   const columns = 7;
+  /* States for the game: */
+  //board states. 6 rows and 7 columns
   const [board, setBoard] = useState<string[][]>(
     Array.from({ length: rows }, () => Array(columns).fill(""))
   );
 
+  //states for players. (X & O)
   const [players, setPlayers] = useState([
     { name: "Player 1", symbol: "X" },
     { name: "Player 2", symbol: "O" },
   ]);
 
+  //state for tracking the player's turn. (0='X'-player, 1='O'-player)
   const [currentPlayerIndex, setCurrentPlayerIndex] = useState(0);
+
+  // gameMode track (PvsP, PvsB - ).
   const [isVsBot, setIsVsBot] = useState(false);
+
+  // state to hold messages
   const [message, setMessage] = useState<string | null>(null);
+
+  //check the win with useWinCheck
   const { winCheck, isFull } = useWinCheck(rows, columns, board);
 
   // reset the board to it's initial state
@@ -34,6 +47,7 @@ export default function Game(props: GameProps) {
     setMessage(null); // reset any messages.
   };
 
+  // useEffect-hook to reset the game when switching btw PvsP and PvsB modes
   useEffect(() => {
     // PvsBot game mode
     if (isVsBot) {
@@ -58,27 +72,32 @@ export default function Game(props: GameProps) {
     }
     //reset the bord when gamemode changes
     resetBoard();
-  }, [isVsBot]);
+  }, [isVsBot]); // useEffect runs whenever isVsBot changes
 
   /* Handle click events - cell click and player moves  */
+  // the function that handle the click event when a cell is clicked
   const handleCellClick = (column: number) => {
-    if (message) return;
-    const currentPlayer = players[currentPlayerIndex];
-
+    const currentPlayer = players[currentPlayerIndex]; // get the current player
+    //check if a column is full before allowing a move.
     if (board[0][column] !== "") {
-      setMessage("This column is full! Try another one!");
+      setMessage("This column is full! Try another one!"); //Displays a message when the column is full, prompting the user to choose another column
       return;
     }
 
     /* function to place a player's symbol in the empty cell */
-    const newBoard = [...board];
-    for (let row = newBoard.length - 1; row >= 0; row--) {
-      if (newBoard[row][column] === "") {
-        newBoard[row][column] = currentPlayer.symbol;
 
+    const newBoard = [...board]; //a copy of the board array not to modify the original board state
+    //loop from bottom to top.iterates upwards to find the lowest empty cell
+    for (let row = newBoard.length - 1; row >= 0; row--) {
+      //check if the selected cell is empty
+      if (newBoard[row][column] === "") {
+        //if it's empty place the player's symbol
+        newBoard[row][column] = currentPlayer.symbol;
+        //when the symbol is placed in the lowest cell, break; the function to prevent further checking
         break;
       }
     }
+
     //update the board state
     setBoard(newBoard);
 
@@ -100,6 +119,7 @@ export default function Game(props: GameProps) {
   const toggleGameMode = () => {
     setIsVsBot(!isVsBot);
   };
+
   return (
     <div>
       <div className="C4-text">
@@ -107,7 +127,7 @@ export default function Game(props: GameProps) {
       </div>
 
       <Board board={board} handleCellClick={handleCellClick} />
-      {message && <p>{message}</p>}
+      {message && <p>{message} </p>}
       <div className="switch">
         <button onClick={toggleGameMode}>
           {isVsBot ? "Switch to PvP" : "Switch to PvB"}
